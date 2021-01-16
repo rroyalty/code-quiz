886895ed57da97266570c1d8b824b3434eb537e5
rroyalty <rroyalty@gmail.com>
2021-01-15 22:07:08 -0500

    Tinkering

--- script.js
+++ script.js
@@ -1,64 +1,66 @@
 
-//JSON Object full of questions and answers. answer[0] always correct answer. Order to be randomized later.
-
 $(document).ready(function() {
-
   // Initial Variables
   let fluidQuestions = quizQuestions;
 
-  // Here we create the on click event that gets the user"s pick
+  // Creates the on click event and executes on Answer Click function.
   $(".btn-choice").on("click", function() {
 
-    
-    const yourPick = $(this).val();
-    const answer = parseInt(yourPick);
+    //Defines clicked button.
+    const yourPick = parseInt($(this).val());
 
-    if (answer === 0) {
-        $(this).addClass("correct");
-    } else {
-        $(this).addClass("incorrect");
-    };
+    //Checks to see if the answer clicked was the correct one.
 
-    //Randomizes the order of the Answer buttons.
-    let arrLoc = [0, 1, 2, 3];
-    let arrNum = [0, 1, 2, 3];
-    arrLoc.forEach(function(elem, i, arr) {
-      let Length = arrNum.length;
-      let rand = Math.floor(Math.random() * Length);
-      arr[i] = arrNum[(rand)];
-      arrNum.splice((rand), 1);
-    }, arrNum);
-
-    // console.log(arrLoc);
-
-    let questionsLeft = fluidQuestions.questions.length;
-    let randomQNum =  Math.floor(Math.random() * questionsLeft);
-    let buttonCollection = $(".btn-choice");
-    let answerCollection = $(".answerChoice");
-
-    // console.log(buttonCollection);
-    // console.log(answerCollection);
-
-    let buttonArray = [].slice.call (buttonCollection);
-    let answerArray = [].slice.call (answerCollection);
-
-    // console.log(buttonArray);
-    // console.log(answerArray);
-
-    $("#quizQuestion").text(fluidQuestions.questions[randomQNum].question);
-    buttonArray.forEach(function(elem, i) {
-      $(elem).val(parseInt(arrLoc[i]));
-      $(answerArray[i]).text(fluidQuestions.questions[randomQNum].answers[arrLoc[i]]);
-    });
-
-    fluidQuestions.questions.splice(randomQNum, 1);
-
-    if (answer === parseInt(0)) {
-      $(this).removeClass("correct");
+    let lockThis = this;
+    if (yourPick === 0) {
+        $(lockThis).addClass("correct");
       } else {
-        $(this).removeClass("incorrect");
+        $(lockThis).addClass("incorrect");
     };
     
-  
+    setTimeout(function(){
+      //Randomizes the order of the Answer buttons.
+      let arrLoc = [0, 1, 2, 3];
+      let arrNum = [0, 1, 2, 3];
+      arrLoc.forEach(function(elem, i, arr) {
+        let Length = arrNum.length;
+        let rand = Math.floor(Math.random() * Length);
+        arr[i] = arrNum[(rand)];
+        arrNum.splice((rand), 1);
+      }, arrNum);
+
+      // Console Log Stuff
+        // console.log(arrLoc);
+
+      // More Variables
+      let questionsLeft = fluidQuestions.questions.length;
+      let randomQNum =  Math.floor(Math.random() * questionsLeft);
+      let buttonCollection = $(".btn-choice");
+      let answerCollection = $(".answerChoice");
+
+      // Turn the collections into Arrays.
+      let buttonArray = [].slice.call (buttonCollection);
+      let answerArray = [].slice.call (answerCollection);
+
+      // Console Log Stuff
+        // console.log(buttonArray);
+        // console.log(answerArray);
+
+      // Sets the next question and removes the current Question from the array.
+      $("#quizQuestion").text(fluidQuestions.questions[randomQNum].question);
+        buttonArray.forEach(function(elem, i) {
+          $(elem).val(parseInt(arrLoc[i]));
+          $(answerArray[i]).text(fluidQuestions.questions[randomQNum].answers[arrLoc[i]]);
+        });
+
+      fluidQuestions.questions.splice(randomQNum, 1);
+
+      // Resets Correct/Incorrect
+      if (yourPick === 0) {
+        $(lockThis).removeClass("correct");
+        } else {
+          $(lockThis).removeClass("incorrect");
+      };
+    }, 100);
   });
 });
