791696f882480172f2494cf5ceda08d511e5c298
rroyalty <rroyalty@gmail.com>
2021-01-16 17:02:41 -0500

    Tinkering with scoring!

--- script.js
+++ script.js
@@ -11,15 +11,20 @@ $(document).ready(function() {
   let timeElapsed = 0;
   let timer = 0;
 
-  //Begins the game
+  // Begins the game
   $(".btn-begin").on("click", function() {
 
+    // Sets initial time to 30 seconds.
     timerAdjust(30);
+
+    // Initiates count down.
     gameTimer();
 
+    // Hides opening screen and shows quiz screen.
     $("#stage1").attr("hidden", true);
     $("#stage2").attr("hidden", false);
 
+    // Picks first question and randomizes answer order.
     fluidQuestions = randomizeButtons(fluidQuestions);
   });
 
@@ -30,14 +35,17 @@ $(document).ready(function() {
     const yourPick = parseInt($(this).val());
 
     //Checks to see if the answer clicked was the correct one.
-    let lockThis = this;
+    let _this = this;
+
+    // Correct Answer: Correct count +1, +3 seconds to timer, flash button Green.
     if (yourPick === 0) {
-        $(lockThis).addClass("correct");
+        $(_this).addClass("correct");
         correctCount++;
         timerAdjust(3);
 
+      // Incorrect Answer: Incorrect count +1, -5 seconds to timer, flash button Red.
       } else {
-        $(lockThis).addClass("incorrect");
+        $(_this).addClass("incorrect");
         incorrectCount++;
         timerAdjust(-5);
     };
@@ -45,17 +53,19 @@ $(document).ready(function() {
     //Delays for a moment to show correct/incorrect formatting.
     setTimeout(function(){
 
+      // Pick next question randomly and randomize answer order.
       fluidQuestions = randomizeButtons(fluidQuestions);
 
-      // Resets Correct/Incorrect
+      // Resets clicked button to original color.
       if (yourPick === 0) {
-        $(lockThis).removeClass("correct");
+        $(_this).removeClass("correct");
         } else {
-          $(lockThis).removeClass("incorrect");
+          $(_this).removeClass("incorrect");
       };
     }, 100);
   });
 
+  // Countdown timer. At time <= 0, iniate GameOver.
   function gameTimer() {
     let interval = setInterval(function() {
       if (parseInt(timer) <= 0) {
@@ -69,11 +79,18 @@ $(document).ready(function() {
 
   };
 
+  // Adjust game timer. The reason this is a seperate function than gameTimer() is because I wanted the display to update to the new time immediately when a user clicks a button instead of waiting for an interval countdown.
   function timerAdjust (adjustment) {
+
+    // Adds (or subtracts for negative parameters) time to timer.
     timer = timer + adjustment;
+
+    //Ends game and stops timer if time <= 0.
     if (parseInt(timer) <= 0) {
       clearInterval(interval);
       gameOver();
+
+      // Updates game timer display. If time is <= 10 seconds, increase font and change color to red.
     } else {
       $("#timer").text("Time Remaining: " + timer);
       if (parseInt(timer) <= 10) {
@@ -84,21 +101,57 @@ $(document).ready(function() {
     };
   };
 
+  // Game over function. Hides quiz screen, shows Game Over screen, records score.
   function gameOver() {
     $("#stage2").attr("hidden", true);
     $("#stage3").attr("hidden", false);
 
+    // If no highscore yet recorded, create a new JSON object and populate.
     if(!highScores) {
-      highScores = {entries: [{player: "",correct: "",incorrect: "",elapsed: ""}]};
-      highScores.entries[0].player = "Ryan Royalty";
-      highScores.entries[0].correct = correctCount;
-      highScores.entries[0].incorrect = incorrectCount;
-      highScores.entries[0].elapsed = timeElapsed;
-    } else {
-      highScores['entries'].push({"player":"Ryan Royalty","correct":correctCount,"incorrect":incorrectCount,"elapsed":timeElapsed});
-    }
+      let player = prompt("Please input your name to record highscore:");
+      if (player != null) {
+        highScores = {entries: [{player: "",correct: "",incorrect: "",elapsed: ""}]};
+        highScores.entries[0].player = player;
+        highScores.entries[0].correct = correctCount;
+        highScores.entries[0].incorrect = incorrectCount;
+        highScores.entries[0].elapsed = timeElapsed;
+      }
 
+      // If highscore present, push new score into JSON object. Sorts by Correct Answers, then Incorrect Answers, then Time Elapsed.
+    } else {
+        for (let i = 0; i < 10; i++) {
+          let wCorrect = parseInt(highScores.entries[i].correctCount);
+          let wIncorrect = parseInt(highScores.entries[i].incorrectCount);
+          let wElapsed = parseInt(highScores.entries[i].timeElapsed);
+
+          switch(true) {
+            case !highScores.entries[i] === true:
+              let player1 = prompt("Please input your name to record highscore:");
+              if (player1 != null) {
+                highScores['entries'].push({"player":player1,"correct":correctCount,"incorrect":incorrectCount,"elapsed":timeElapsed});
+              }
+              i = 11;
+              break;
+            case wCorrect > parseInt(correctCount):
+              break;
+            case wCorrect === parseInt(correctCount) && wIncorrect < parseInt(incorrectCount):
+              break;
+            case wCorrect === parseInt(correctCount) && wIncorrect === parseInt(incorrectCount) && wElapsed < parseInt(timeElapsed):
+              break;
+            default:
+              let player2 = prompt("Please input your name to record highscore:");
+              if (player2 != null) {
+                highScores['entries'].splice(i + 1, 0, {"player":player2,"correct":correctCount,"incorrect":incorrectCount,"elapsed":timeElapsed});
+              }
+              i = 11;
+          }
+        }
+      }
+
+    // Local storage JSON object.
+    highScores['entries'].splice(11,1);
     localStorage.setItem("highScores", JSON.stringify(highScores));
+    console.log(highScores);
 
   };
 
