e6087853045abef83c757a929fb7e9f127bcf3bb
rroyalty <rroyalty@gmail.com>
2021-01-17 09:11:20 -0500

    Aesthetics

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-  $("#stage2").attr("hidden", true);
-  $("#stage3").attr("hidden", true);
-  $("#stage1").attr("hidden", false);
-
   // Initial Variables
   let fluidQuestions = quizQuestions;
   let highScores = JSON.parse(localStorage.getItem("highScores"));
@@ -10,70 +6,50 @@ $(document).ready(function() {
   let incorrectCount = 0;
   let timeElapsed = 0;
   let timer = 0;
-  let playerCollection = $(".playerList");
-  let correctCollection = $(".correctGuesses");
-  let incorrectCollection = $(".incorrectGuesses");
-  let elapsedCollection = $(".timeElapsed");
-    // Turn the collections into Arrays.
-    let playerArray = [].slice.call (playerCollection);
-    let correctArray = [].slice.call (correctCollection);
-    let incorrectArray = [].slice.call (incorrectCollection);
-    let elapsedArray = [].slice.call (elapsedCollection);
 
-    if (highScores) {
-      let hsLength = highScores.entries.length;
-      for(let i = 0; i < hsLength; i++) {
-        playerArray[i].innerText = highScores.entries[i].player;
-        correctArray[i].innerText = highScores.entries[i].correct;
-        incorrectArray[i].innerText = highScores.entries[i].incorrect;
-        elapsedArray[i].innerText = highScores.entries[i].elapsed;
-      }
-    }
-  // Begins the game
+  let playerArray = [].slice.call ($(".playerList"));
+  let correctArray = [].slice.call ($(".correctGuesses"));
+  let incorrectArray = [].slice.call ($(".incorrectGuesses"));
+  let elapsedArray = [].slice.call ($(".timeElapsed"));
+
+  // Populates high scores.
+  populateHS();
+
+  // BUTTON: Begins the game
   $(".btn-begin").on("click", function() {
-
     // Sets initial time to 30 seconds.
     timerAdjust(30);
-
     // Initiates count down.
     gameTimer();
-
     // Hides opening screen and shows quiz screen.
     $("#stage1").attr("hidden", true);
     $("#stage2").attr("hidden", false);
-
     // Picks first question and randomizes answer order.
     fluidQuestions = randomizeButtons(fluidQuestions);
   });
+  // END BUTTON
 
-  // Creates the on click event and executes on Answer Click function.
+  // BUTTON: Selects user answer, checks if it's correct and applies appropriate logic, populate next question and answers.
   $(".btn-choice").on("click", function() {
-
     //Defines clicked button.
     const yourPick = parseInt($(this).val());
-
     //Checks to see if the answer clicked was the correct one.
     let _this = this;
-
     // Correct Answer: Correct count +1, +3 seconds to timer, flash button Green.
     if (yourPick === 0) {
         $(_this).addClass("correct");
         correctCount++;
         timerAdjust(3);
-
       // Incorrect Answer: Incorrect count +1, -5 seconds to timer, flash button Red.
       } else {
         $(_this).addClass("incorrect");
         incorrectCount++;
         timerAdjust(-5);
-    };
-    
+        };
     //Delays for a moment to show correct/incorrect formatting.
     setTimeout(function(){
-
       // Pick next question randomly and randomize answer order.
       fluidQuestions = randomizeButtons(fluidQuestions);
-
       // Resets clicked button to original color.
       if (yourPick === 0) {
         $(_this).removeClass("correct");
@@ -82,32 +58,28 @@ $(document).ready(function() {
       };
     }, 100);
   });
+  // END BUTTON
 
-  // Countdown timer. At time <= 0, iniate GameOver.
+  // FUNCTION: Countdown timer. At time <= 0, iniate GameOver.
   function gameTimer() {
     let interval = setInterval(function() {
       if (parseInt(timer) <= 0) {
         clearInterval(interval);
-        gameOver();
       } else {
           timeElapsed++;
           timerAdjust (-1);
         }
     }, 1000);
-
   };
+  // END FUNCTION
 
-  // Adjust game timer. The reason this is a seperate function than gameTimer() is because I wanted the display to update to the new time immediately when a user clicks a button instead of waiting for an interval countdown.
+  // FUNCTION: Adjust game timer. The reason this is a seperate function than gameTimer() is because I wanted the display to update to the new time immediately when a user clicks a button instead of waiting for an interval countdown.
   function timerAdjust (adjustment) {
-
     // Adds (or subtracts for negative parameters) time to timer.
     timer = timer + adjustment;
-
     //Ends game and stops timer if time <= 0.
     if (parseInt(timer) <= 0) {
-      clearInterval(interval);
       gameOver();
-
       // Updates game timer display. If time is <= 10 seconds, increase font and change color to red.
     } else {
       $("#timer").text("Time Remaining: " + timer);
@@ -118,11 +90,12 @@ $(document).ready(function() {
         };
     };
   };
+  // END FUNCTION
 
-  // Game over function. Hides quiz screen, shows Game Over screen, records score.
+  // FUNCTION: Game over function. Hides quiz screen, shows Game Over screen, records score.
   function gameOver() {
     $("#stage2").attr("hidden", true);
-    $("#stage3").attr("hidden", false);
+    $("#stage1").attr("hidden", false);
 
     // If no highscore yet recorded, create a new JSON object and populate.
     if(!highScores) {
@@ -138,17 +111,20 @@ $(document).ready(function() {
       // If highscore present, push new score into JSON object. Sorts by Correct Answers, then Incorrect Answers, then Time Elapsed.
     } else {
         for (let i = 0; i < 10; i++) {
+          // If Entry is Null, Push entry into array.
           if ( !highScores.entries[i] ) {
             let player1 = prompt("Please input your name to record highscore:");
             if (player1 != null) {
               highScores['entries'].push({"player":player1,"correct":correctCount,"incorrect":incorrectCount,"elapsed":timeElapsed});
             }
             i = 11;
+            // Sorts current entry into array based on Correct Answers, Incorrect Answers, and Seconds Elapsed.
           } else {
+              // Variables parsed as Integers.
               let wCorrect = parseInt(highScores.entries[i].correct);
               let wIncorrect = parseInt(highScores.entries[i].incorrect);
               let wElapsed = parseInt(highScores.entries[i].elapsed);
-
+              // Switch logic.
               switch(true) {
                 case wCorrect > parseInt(correctCount):
                   break;
@@ -166,35 +142,23 @@ $(document).ready(function() {
             }
         }
       }
-
     // Local storage JSON object.
     highScores['entries'].splice(11,1);
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    console.log(highScores);
-
+    // Populate High Score.
+    populateHS();
   };
+  // END FUNCTION
 
-  $(".btn-again").on("click", function() {
-   // Hides opening screen and shows quiz screen.
-    $("#stage2").attr("hidden", true);
-    $("#stage1").attr("hidden", false);
-    location.reload();
-  });
-
-  // An array of the unused questions is passed in to this function, which then selects a random question from that array,splices it out so it won't be seen again, and randomizes the order of the answers seen on screen
+  // FUNCTION :An array of the unused questions is passed in to this function, which then selects a random question from that array,splices it out so it won't be seen again, and randomizes the order of the answers seen on screen
   function randomizeButtons(questionsArr) {
-
     // Function variables
     let questionsLeft = questionsArr.questions.length;
     let randomQNum =  Math.floor(Math.random() * questionsLeft);
     let arrLoc = [0, 1, 2, 3];
     let arrNum = [0, 1, 2, 3];
-    let buttonCollection = $(".btn-choice");
-    let answerCollection = $(".answerChoice");
-      // Turn the collections into Arrays.
-      let buttonArray = [].slice.call (buttonCollection);
-      let answerArray = [].slice.call (answerCollection);
-
+    let buttonArray = [].slice.call ($(".btn-choice"));
+    let answerArray = [].slice.call ($(".answerChoice"));
     // Randomizes the order of arrLoc in order to randomize the order of the buttons on the screen.
     arrLoc.forEach(function(elem, i, arr) {
       let Length = arrNum.length;
@@ -202,7 +166,6 @@ $(document).ready(function() {
       arr[i] = arrNum[(rand)];
       arrNum.splice((rand), 1);
     }, arrNum);
-
     // 1. Sets the next question and removes the current Question from the array.
     // 2. Sets the answers in a random order.
     $("#quizQuestion").text(questionsArr.questions[randomQNum].question);
@@ -210,10 +173,25 @@ $(document).ready(function() {
       $(elem).val(parseInt(arrLoc[i]));
       $(answerArray[i]).text(questionsArr.questions[randomQNum].answers[arrLoc[i]]);
     });
-
+    // Removes used questions from the question pool.
     questionsArr.questions.splice(randomQNum, 1);
-      return questionsArr;
+    return questionsArr;
   };
+  // END FUNCTION
+
+  // FUNCTION: Populate the high score table.
+  function populateHS() {
+    if (highScores) {
+      let hsLength = highScores.entries.length;
+      for(let i = 0; i < hsLength; i++) {
+        playerArray[i].innerText = highScores.entries[i].player;
+        correctArray[i].innerText = highScores.entries[i].correct;
+        incorrectArray[i].innerText = highScores.entries[i].incorrect;
+        elapsedArray[i].innerText = highScores.entries[i].elapsed;
+      }
+    }
+  }
+  // END FUNCTION
 
 });
 
